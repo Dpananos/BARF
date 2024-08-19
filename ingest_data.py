70c2dd766efaf5d18a06eef7ee3edfac3e12687f
Demetri Pananos <dpananos@gmail.com>
2024-08-19 01:35:50 -0400

    use duckdb globbing to read in data

--- ingest_data.py
+++ ingest_data.py
@@ -1,3 +1,4 @@
+import dotenv
 import os
 import json
 import duckdb
@@ -12,55 +13,26 @@ logging.basicConfig(level=logging.INFO,
                               logging.StreamHandler()])
 
 
-def ingest_data_fact_tips(conn, file: str):
-    logging.info(f"Starting data ingestion for file: {file}")
-    try:
-        logging.info("Connected to the DuckDB database")
-
-        conn.sql("CREATE TABLE IF NOT EXISTS staging_fact_trips (filename VARCHAR, datetime DATETIME, station_trips BIGINT)")
-
-        df = pd.read_json(file)
-        
-        # File will serve as our unique identifier.
-        df['filename'] = file
-        # API returns "station trips" -- with a space.
-        df.columns = [col_name.replace(' ', '_') for col_name in df.columns]
-        df = df[['filename','datetime','station_trips']]
-
-        result = conn.execute(f"SELECT COUNT(*) FROM staging_fact_trips WHERE filename == '{file}'").fetchall()[0][0]
-        logging.info(f"Number of existing records for {file}: {result}")
-
-        if result == 0:
-            conn.sql("INSERT INTO staging_fact_trips SELECT * FROM df")
-            logging.info("Data inserted into staging_fact_trips table")
-            conn.commit()
-        else:
-            logging.info("Record already ingested, not inserting new data.")
-
-    except Exception as e:
-        logging.error(f"Error during data ingestion for file {file}: {e}")
-
-
-def crawl_directory(directory, conn):
-    """Crawl through the directory and process JSON files."""
-    logging.info(f"Starting to crawl directory: {directory}")
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.json'):
-                file_path = os.path.join(root, file)
-                logging.info(f"Processing file: {file_path}")
-                ingest_data_fact_tips(conn=conn, file=file_path)
+def ingest_data_fact_tips(conn, data_dir: str):
+    conn.execute("create database if not exists bike")
+    conn.execute("use bike")
+    conn.execute("create schema if not exists raw")
+    conn.execute(f"create or replace table raw.raw_trip_data as select * from read_json('{data_dir}/*/*/*/data.json', format='array')")
+    conn.commit()
 
 
 if __name__ == '__main__':
+
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Ingest JSON data into a DuckDB database.")
-    parser.add_argument('--db', default='data.db', help="Path to the DuckDB database file. Default is 'data.db'.")
-    parser.add_argument('--root_dir', default='data/', help="Path to the root directory containing JSON files. Default is 'data/'.")
+    parser.add_argument('--root_dir', default='data', help="Path to the root directory containing JSON files. Default is 'data'.")
 
     args = parser.parse_args()
 
-    # Execute the main functionality
-    with duckdb.connect(database=args.db, read_only=False) as conn:
-        crawl_directory(args.root_dir, conn)
+    dotenv.load_dotenv('.env')
+    motherduck_token = os.getenv('motherduck_token')
+    my_string = f"md:?motherduck_token={motherduck_token}"
+        
+    with duckdb.connect(my_string) as conn:
+        ingest_data_fact_tips(conn, args.root_dir)
         logging.info("Data ingestion process completed")
