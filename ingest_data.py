c0b4b5df3dd79cd0e5db4fb771aad5117dc09f3a
Demetri Pananos <dpananos@gmail.com>
2024-09-05 20:31:48 -0400

    single ingest

--- ingest_data.py
+++ ingest_data.py
@@ -14,12 +14,20 @@ logging.basicConfig(
 )
 
 
-def ingest_data_fact_tips(conn, data_dir: str):
+def ingest_weather_data(conn, data_dir: str):
     conn.execute("create database if not exists bike")
     conn.execute("use bike")
     conn.execute("create schema if not exists raw")
     conn.execute(
         f"create or replace table raw.raw_weather_data as (select *, now() as _etl_loaded_at from read_json('{data_dir}/*/*/*/weather-data.json'))"
+    )
+
+    conn.execute(
+        f"create or replace table raw.raw_weather_forecast_Data as (select *, now() as _etc_loaded_at from read_json('{data_dir}/weather_forecasts/*.json'))"
+    )
+
+    conn.execute(
+        f"create or replace table raw.raw_trip_data as  (select *, now() as _etl_loaded_at from read_json('{data_dir}/*/*/*/data.json', format='array'))"
     )
     conn.commit()
 
@@ -42,5 +50,5 @@ if __name__ == "__main__":
     my_string = f"md:?motherduck_token={motherduck_token}"
 
     with duckdb.connect(my_string) as conn:
-        ingest_data_fact_tips(conn, args.root_dir)
+        ingest_weather_data(conn, args.root_dir)
         logging.info("Data ingestion process completed")
